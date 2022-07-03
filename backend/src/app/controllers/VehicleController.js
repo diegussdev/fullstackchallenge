45796df33886df49bb4a5425d3dbdadc56088a51
diegussdev <diegussdev@gmail.com>
2022-07-03 16:07:48 -0300

    Adiciona CRUD e rotas

--- backend/src/app/controllers/VehicleController.js
+++ backend/src/app/controllers/VehicleController.js
@@ -2,6 +2,25 @@ const Vehicle = require("../models/Vehicle");
 
 class VehicleController {
 
+    async store(req, res) {
+        try {
+            var vehicle = await Vehicle.create(req.body)
+        } catch (error) {
+            if (error['name'] == "SequelizeValidationError") {
+                const errObj = {};
+                error.errors.map( er => {
+                    errObj[er.path] = er.message;
+                })
+                
+                return res.json(errObj, 422)
+            }
+            
+            return res.json(["Unexpected Error."], 500)
+        }
+
+        return res.json(vehicle, 201);
+    }
+
     async index(req, res) {
         const page = req.query.page ? req.query.page : 1
         
@@ -30,6 +49,61 @@ class VehicleController {
         }
 
         return res.json(response)
+    }
+
+    async find(req, res) {
+        const id = req.params.id
+
+        try {
+            var vehicles = await Vehicle.findByPk(id)
+        } catch (error) {
+            return res.json(["Unexpected Error."], 500)
+        }
+
+        if (!vehicles) {
+            return res.status(404).send()
+        }
+        
+        return res.json(vehicles)
+    }
+
+    async update(req, res) {
+        const id = req.params.id
+        const options = {
+            where: { id }
+        }
+
+        try {
+            await Vehicle.update(req.body, options)
+        } catch (error) {
+            if (error['name'] == "SequelizeValidationError") {
+                const errObj = {};
+                error.errors.map( er => {
+                    errObj[er.path] = er.message;
+                })
+                
+                return res.json(errObj, 422)
+            }
+
+            return res.json(["Unexpected Error."], 500)
+        }
+
+        return res.status(204).send()
+    }
+
+    async delete(req, res) {
+        const id = req.params.id
+        const options = {
+            where: { id }
+        }
+
+        try {
+            await Vehicle.destroy(options)
+        } catch (error) {
+            return res.json(["Unexpected Error."], 500)
+        }
+
+        return res.status(204).send()
     }
 
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,10 @@ const VehicleController = require('./app/controllers/VehicleController');
 
 const routes = new Router();
 
+routes.post('/vehicles', VehicleController.store)
 routes.get('/vehicles', VehicleController.index)
+routes.get('/vehicles/:id', VehicleController.find)
+routes.put('/vehicles/:id', VehicleController.update)
+routes.delete('/vehicles/:id', VehicleController.delete)
 
 module.exports = routes;
